2726bf30201459860f1c8ed6e097d1cd0f809986
+kelvin <kevin.mutua@student.moringaschool.com>
2022-11-22 04:14:42 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+ 
+function destructivelyAppendCat(name) {
+    cats.push("Ralph")
+    
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat() {
+    cats.pop("Garfield")
+    return cats
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift("Milo")
+    return cats
+}
+function appendCat(Ralph){
+    var newArray = cats.slice();
+    newArray.push(Ralph)
+    return newArray
+}
+function prependCat(Ralph){
+    var newArray = cats.slice();
+    newArray.unshift(Ralph)
+    return newArray
+}
+function removeLastCat() {
+    let newArray=[]
+    return newArray=cats.slice(0,-1)
+}
+function removeFirstCat() {
+    let newArray=[]
+    return newArray=cats.slice(1)
+}
